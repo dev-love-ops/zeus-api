5af3604174ba5c068258d5492d0af7419c349ec9
RockyWu <rocky@RockyWudeMacBook-Pro.local>
2019-07-15 21:46:46 +0800

    完成用户的查询接口

--- sql/system.sql
+++ sql/system.sql
@@ -3,7 +3,7 @@ CREATE TABLE `system_user` (
   `username` varchar(16) NOT NULL DEFAULT '' COMMENT '用户名',
   `user_id` varchar(16) NOT NULL DEFAULT '' COMMENT '用户ID',
   `mobile` varchar(11) NOT NULL DEFAULT '' COMMENT '手机号',
-  `email` varchar(11) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `email` varchar(32) NOT NULL DEFAULT '' COMMENT '邮箱',
   `password` varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id_index` (`user_id`)
